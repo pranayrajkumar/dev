98c112a1b3ed01609a0e981dde844ccc19ef01a7
unknown <pranay.bandanadam96@gmail.com>
2025-08-06 19:25:24 +0530

    ade change to instance

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "pranay" {
+  ami           = "ami-0d54604676873b4ec"
+  instance_type = "t2.micro"
+  subnet_id     =   "subnet-003cfbbd4cf753c77"
+  tags = {
+    Name = "sample"
+  }
+}
