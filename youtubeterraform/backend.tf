b1e459e0274a7b9cf0e60f52823ffce2a45e9292
pranayrajkumar <32105011+pranayrajkumar@users.noreply.github.com>
2025-08-07 13:21:16 +0530

    deleted the repo

--- youtubeterraform/backend.tf
+++
@@ -1,8 +0,0 @@
-/*terraform {
-    backend "s3" {
-        bucket         = "pranayrajkumar-terraform-bucket1"
-        key            = "youtubeterraform/terraform.tfstate"
-        region         = "ap-south-1"
-        //dynamodb_table = "demo-terraform-eks-state-table-raj"
-    }
-}*/
